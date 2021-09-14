f3b7c3269216c2f9e69da1ddbdbe8f241cfc4292
X <70487315+XWasHere@users.noreply.github.com>
2021-09-14 12:28:01 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,32 +4,47 @@
 #include <termios.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <poll.h>
+#include "ssh.h"
 
 struct _args {
-    int dns;
-    int dns_port;
+    int ftp;
+    int ftp_port;
+    int ssh;
+    int ssh_port;
+    struct sockaddr_in ftp_addr;
+    struct sockaddr_in ssh_addr;
 };
-
 typedef struct _args args_t;
-
-extern void print(const char *, ...);
 
 int main(int argc, char** argv) {
     args_t args;
+    memset(&args, 0, sizeof(args));
     char*  addr;
-    int    dns_sock;
-
+    
     // defaults
-    args.dns      = 0;
-    args.dns_port = 53;
+    args.ftp      = 0;
+    args.ftp_port = 53;
 
     // parsing
     addr = argv[1];
 
-    for (int i = 2; i < argc; i++) {
-        if (strcmp(argv[i], "--dns") == 0) {
-            args.dns = 1; i++;
-            sscanf(argv[i],"%i", &args.dns_port);
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "--ftp") == 0) {
+            args.ftp = 1; i++;
+            sscanf(argv[i],"%i", &args.ftp_port);
+        } else if (strcmp(argv[i], "--ssh") == 0) {
+            args.ssh = 1; i++;
+            sscanf(argv[i],"%i", &args.ssh_port);
+            args.ssh_addr.sin_port = htons(args.ssh_port);
+            printf("SSH_PORT %i\r\n", args.ssh_port);
+        } else if (strcmp(argv[i], "--ipv4") == 0) {
+            i++;
+            args.ssh_addr.sin_family = AF_INET;
+            inet_pton(AF_INET, argv[i], &args.ssh_addr.sin_addr);
+            inet_pton(AF_INET, argv[i], &args.ftp_addr.sin_addr);
         }
     }
 
@@ -41,6 +56,7 @@ int main(int argc, char** argv) {
     tcgetattr(0, &term);
 
     cfmakeraw(&term);
+    setvbuf(stdout, NULL, _IONBF, 0);
 
     tcsetattr(0, 0, &term);
 
@@ -56,38 +72,63 @@ int main(int argc, char** argv) {
     /*
         this actually deserves a bit of explaining, so basically
         the shell has multiple "modes" it can be in.
-        0 - shell command (like the thing you get when you press esc in vi)
-        1 - dns (you get it)
+        0 - internal command (like the thing you get when you press esc in vi)
+        1 - normal (you get it)
     */
     int    mode;
 
-    /*
-        basically just a backup of mode for exiting mode 0
-    */
-    int    retmode;
-
     mode = 0;
-    retmode = 0;
+
+    int ftp_connected, ftp_giveup, ftp_hassock, ftp_sock;
+    int ssh_connected, ssh_giveup, ssh_hassock, ssh_sock;
+    int stdin_fd, ssh_fd;
+
+    // fds
+    struct pollfd connections[32];
+    memset(&connections, 0, sizeof(connections));
+
+    int    fdtype[32];
+
+    connections[0].fd     = STDIN_FILENO;
+    connections[0].events = POLLIN;
+    connections[1].fd     = socket(AF_INET, SOCK_STREAM, 0);
+    connections[1].events = POLLIN;
+
+    ssh_t *ssh_session;
+
+    printf("PHASE: CONNECT\r\n");
+    if (args.ssh) {
+        ssh_sock = connections[1].fd;
+        
+        if (connect(ssh_sock, (struct sockaddr *)&args.ssh_addr, sizeof(args.ssh_addr)) < 0) {
+            printf("Unable to connect to remote host [ERROR %i IN connect()]\r\n", errno);
+            goto ssh_connect_die;
+        }
+
+        ssh_session = ssh(ssh_sock);
+    }
+
+    ssh_connect_die:
 
     // main loop
     while (1) {
-        // this is HORRIBLE. WHY DO YOUNEED TO INPUT TO GET OUHOASHJCNKASLDKJCNALSKDNFUCKYOUX
-        sc_len = read(0, &sc, sizeof(sc));
-        for (int i = 0; i < sc_len; i++) {
-            if ((sc[i] == 194) && (sc[i] == 155)) {
-                // SHIFT-ESC: the key to do the thing idk its like escape in vi
-                if (mode == 0) {
-                    mode = retmode;
-                } else {
-                    retmode = mode;
-                    mode = 0;
-                    // now we're ready to do stuff like configure the shell uwu
+        poll(connections, 2, 100);
+
+        if (connections[0].revents & POLLIN) {
+            memset(&sc, 0, sizeof(sc));
+            sc_len = read(STDIN_FILENO, &sc, sizeof(sc) - 1);
+            for (int i = 0; i < sc_len; i++) {
+                if (sc[i] == 3) {
+                    goto leave;
                 }
-            } else if (sc[i] == 3) {
-                // loks like we need to exit early
-                goto leave;
-            } else {
-                print("%i ", sc[i]);
+            }
+        }
+
+        if (connections[1].revents & POLLIN) {
+            memset(&sc, 0, sizeof(sc));
+            sc_len = read(ssh_sock, &sc, sizeof(sc));
+            for (int ii = 0; ii < sc_len; ii++) {
+                printf("%c ", sc[ii]);
             }
         }
     }
