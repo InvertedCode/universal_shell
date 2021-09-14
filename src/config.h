57f2054067968c8aa6a2d84651fcd7ca924e8b55
X <70487315+XWasHere@users.noreply.github.com>
2021-09-14 12:27:39 -0400

    i still dont have access to git on this device so i have to commit everything one file at a time

--- src/config.h
+++ src/config.h
@@ -1,6 +1,18 @@
 #ifndef _CONFIG_H
 #define _CONFIG_H
 
-#define HAS_DNS
+#include "util.h"
+
+// This is ONLY here for if the protocol is changed. PLEASE DO NOT CHANGE IT UNLESS YOU KNOW WHAT YOURE DOING.
+#define SSH_VERSION "2.0"
+
+// This is the version that is sent to the SSH server
+#define SSH_IDENT "Universal_Shell"
+
+// This is the comment added to the version string
+#define SSH_COMMENT "femboy shell"
+
+// As per RFC-4253
+#define SSH_PROTO_STR "SSH-"SSH_VERSION"-"SSH_IDENT" "SSH_COMMENT"\r\n"
 
 #endif
