a496f1d85e6b4864b2752e72895e93719e4df272
xwashere <70487315+XWasHere@users.noreply.github.com>
2021-09-15 19:24:59 -0400

    is now able to connect to the ssh server. (cant do anything after that though).

--- src/main.c
+++ src/main.c
@@ -1,3 +1,33 @@
-int main(int argc, char** argv) {
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(int argc, char** argv) {
+	// this is currently compiled in, but once it actually works that will change uwu
+	char *ssh_host = "127.0.0.1";
+	int   ssh_port = 22;
+
+	// some other vars
+	int ssh_sock;
+	struct sockaddr_in ssh_addr;
+
+	// open the socket
+	if((ssh_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("error in scoket(): %i\n", errno);
+		exit(-1);
+	}
+
+	// put some junk in this structure
+	ssh_addr.sin_family=AF_INET;
+	ssh_addr.sin_addr.s_addr = inet_addr(ssh_host);
+	ssh_addr.sin_port = htons(ssh_port);
+
+	// connect to the server
+	if(connect(ssh_sock, (struct sockaddr *)&ssh_addr, sizeof(ssh_addr)) < 0) {
+		printf("error in connect(): %i\n", errno);
+		exit(-1);
+	};
 }
